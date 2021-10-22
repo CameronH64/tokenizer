e77e5dda33d82a69697b312e306bb32d68d1c86e
CameronH926 <cholbrook@cub.uca.edu>
2021-10-22 14:08:28 -0500

    Getting some of the necessary functions.

--- main.py
+++ main.py
@@ -9,9 +9,23 @@ reserved words
 constants
 identifiers
 
+PSEUDOCODE:
+
+    Read character by character
+
+    Each time a character is read, see if it's a lexeme.
+
+    If it's a lexeme, identify the token
+    Then, output the lexeme (with labels) to .xml.
+
+
+
+    Else, continue with reading characters.
+
+    Do this until end of file is reached.
 
 """
-import symtable
+
 
 whileSpaceandCommentsDictionary = { 'WSaC': ['//', '/*', '*/', '/**'] }
 symbolsDictionary = { 'symbols': ['(', ')', '[', ']', '{', '}', ',', ';', '=', '.', '+', '-', '*', '/', '&', '|', '~', '<', '>'] }
@@ -34,20 +48,51 @@ def identify_token(lexeme):
 
     # Need to have one for comments, but DON'T write to the .xml file.
 
+def write_token(token):
 
+
+def read_lexeme():
+
+    currentLexeme = ""          # Start with an empty lexeme.
+
+    while 1:
+
+        # read character by character.
+        char = file.read(1)
+        if not char:  # If you reach the end of the file, break out of the loop.
+            break
+
+        currentLexeme = currentLexeme + char  # Concatenate the just-read char to the end of the file.
+
+        digit = currentLexeme[0].isdigit()
+
+        if digit:
+            continue
+        else:
+
+        # Do an identify_lexeme here somewhere.
+
+        currentLexeme = ""      # Reset the currentLexeme.
+
+        return lexeme
+
+
+
+################## Code Execution Start
 
 file = open('Main.jack', 'r')           # Open file in read mode.
 
-currentLexeme = ""
+endOfFile = False
 
-while 1:
+while not endOfFile:
+    read_lexeme()
 
-    # read character by character.
-    char = file.read(1)
-    if not char:        # If you reach the end of the file, break out of the loop.
-        break
 
-    currentLexeme.append(char)
-    print(currentLexeme)
 
 file.close()
+
+
+
+
+
+
